b4f0c6968501add3ad14d9fa60f224bc97084bb2
Odile078 <ouwimpuhwe620@daviscollege.com>
2021-01-09 13:11:03 +0200

    creating the popular movies list activity

---
+++ app/src/main/java/com/example/mvstudio/PopularMovieListActivity.java
@@ -0,0 +1,14 @@
+package com.example.mvstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopularMovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular_movie_list);
+    }
+}
