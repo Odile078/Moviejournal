81f08af961b0d04ab55801653b137af522ab52d0
Odile078 <ouwimpuhwe620@daviscollege.com>
2021-01-09 20:18:24 +0200

    >creating the detail activity

---
+++ app/src/main/java/com/example/mvstudio/PopularMovieDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.mvstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopularMovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular_movie_detail);
+    }
+}
